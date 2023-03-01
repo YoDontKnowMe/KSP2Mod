ec90d9f86954557664ef194ea8268f4a20a231f7
Derplex <derp.plex@gmail.com>
2023-03-01 18:31:24 +0100

    Third upload

---
+++ Extra Stuff/GameData/Mods/TestMod/update.lua
@@ -0,0 +1,9 @@
+function start ()
+	Debug.Log("YAAAAAAAAAAAAAAAAAAAAAY")
+end
+function update ()
+	Debug.Log("YAAAAAAAAAAAAAAAAAAAAAY")
+end
+function shutdown ()
+	Debug.Log("YAAAAAAAAAAAAAAAAAAAAAY")
+end
